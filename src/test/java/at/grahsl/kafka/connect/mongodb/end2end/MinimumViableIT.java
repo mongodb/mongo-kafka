0e1c2138463a48ee6b93c4a549a1154f253c0c60
hpgrahsl <hpgrahsl@users.noreply.github.com>
2017-11-21 21:37:28 +0100

    add pom dependencies, simplify test case scenario
    update docker compose image versions
    integrate yaml parser to read host/port settings from docker compose file

--- src/test/java/at/grahsl/kafka/connect/mongodb/end2end/MinimumViableIT.java
+++ src/test/java/at/grahsl/kafka/connect/mongodb/end2end/MinimumViableIT.java
@@ -17,6 +17,7 @@
 package at.grahsl.kafka.connect.mongodb.end2end;
 
 import at.grahsl.kafka.connect.mongodb.data.avro.TweetMsg;
+import com.esotericsoftware.yamlbeans.YamlReader;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
@@ -36,6 +37,7 @@ import org.junit.runner.RunWith;
 import org.testcontainers.containers.DockerComposeContainer;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -43,29 +45,28 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 @RunWith(JUnitPlatform.class)
-public class ContainerTest {
+public class MinimumViableIT {
 
     public static final String DOCKER_COMPOSE_FILE = "src/test/resources/docker/compose-env.yml";
     public static final String DEFAULT_COMPOSE_SERVICE_SUFFIX = "_1";
 
     public static final String SINK_CONNECTOR_CONFIG = "src/test/resources/config/sink_connector.json";
 
-    //TODO: read these host:port settings from .yml compose file
+    public static final String KAFKA_BROKER;
+    public static final int KAFKA_BROKER_PORT;
 
-    public static final String KAFKA_BROKER = "kafkabroker";
-    public static final int KAFKA_BROKER_PORT = 9092;
+    public static final String KAFKA_CONNECT;
+    public static final int KAFKA_CONNECT_PORT;
 
-    public static final String KAFKA_CONNECT = "kafkaconnect";
-    public static final int KAFKA_CONNECT_PORT = 8083;
+    public static final String SCHEMA_REGISTRY;
+    public static final int SCHEMA_REGISTRY_PORT;
 
-    public static final String SCHEMA_REGISTRY = "schemaregistry";
-    public static final int SCHEMA_REGISTRY_PORT = 8081;
-
-    public static final String MONGODB = "mongodb";
-    public static int MONGODB_PORT = 27017;
+    public static final String MONGODB;
+    public static int MONGODB_PORT;
 
     public static long PROCESSING_DELAY_TIME_MILLIS = 5_000L;
 
@@ -74,6 +75,26 @@ public class ContainerTest {
     private static MongoDatabase MONGO_DATABASE;
 
     private static KafkaProducer<String, TweetMsg> PRODUCER;
+
+    static {
+        try {
+            Map composeFile = (Map)new YamlReader(new FileReader(DOCKER_COMPOSE_FILE)).read();
+
+            KAFKA_BROKER = extractHostnameFromDockerCompose(composeFile,"kafkabroker");
+            KAFKA_BROKER_PORT = extractHostPortFromDockerCompose(composeFile,"kafkabroker");
+
+            KAFKA_CONNECT = extractHostnameFromDockerCompose(composeFile,"kafkaconnect");
+            KAFKA_CONNECT_PORT = extractHostPortFromDockerCompose(composeFile,"kafkaconnect");
+
+            SCHEMA_REGISTRY = extractHostnameFromDockerCompose(composeFile,"schemaregistry");
+            SCHEMA_REGISTRY_PORT = extractHostPortFromDockerCompose(composeFile,"schemaregistry");
+
+            MONGODB = extractHostnameFromDockerCompose(composeFile,"mongodb");
+            MONGODB_PORT = extractHostPortFromDockerCompose(composeFile,"mongodb");
+        } catch(Exception exc) {
+            throw new RuntimeException("error: parsing the docker-compose YAML",exc);
+        }
+    }
 
     @ClassRule
     public static DockerComposeContainer CONTAINER_ENV =
@@ -104,45 +125,39 @@ public class ContainerTest {
         registerMongoDBSinkConnector(config);
     }
 
-    @RepeatedTest(value=100, name = "{displayName} {currentRepetition}/{totalRepetitions}")
-    @DisplayName("producing avro records to kafka topic")
-    public void produceKafkaAvroRecords(RepetitionInfo info) {
+    @Test
+    @DisplayName("when producing kafka records then verify saved mongodb documents")
+    public void produceKafkaRecordsAndVerifyMongoDbDocuments() {
 
-        int tweetCnt = info.getCurrentRepetition();
+        int numTestRecords = 100;
+        for (int tid = 0; tid < numTestRecords; tid++) {
+            TweetMsg tweet = TweetMsg.newBuilder()
+                    .setId$1(tid)
+                    .setText("test tweet " + (tid) + ": end2end testing apache kafka <-> mongodb sink connector is fun!")
+                    .setHashtags(Arrays.asList(new String[]{"t" + tid, "kafka", "mongodb", "testing"}))
+                    .build();
 
-        TweetMsg tweet = TweetMsg.newBuilder()
-                .setId$1(tweetCnt)
-                .setText("test tweet "+(tweetCnt)+": end2end testing apache kafka <-> mongodb sink connector is fun!")
-                .setHashtags(Arrays.asList(new String[]{"t"+tweetCnt,"kafka","mongodb","testing"}))
-                .build();
+            ProducerRecord<String, TweetMsg> record = new ProducerRecord<>("e2e-test-topic", tweet);
+            System.out.println(LocalDateTime.now() + " producer sending -> " + tweet.toString());
 
-        ProducerRecord<String, TweetMsg> record = new ProducerRecord<>("e2e-test-topic", tweet);
-        System.out.println(LocalDateTime.now() + " producer sending -> " + tweet.toString());
-
-        PRODUCER.send(record, (RecordMetadata r, Exception exc) -> {
-            assertNull(exc, () -> "unexpected error while sending: " + tweet
-                                        + " | exc: "+exc.getMessage()
-            );
-        });
-
-    }
-
-    @RepeatedTest(value = 100, name = "{displayName} {currentRepetition}/{totalRepetitions}")
-    @DisplayName("looking up expected records in MongoDB collection")
-    public void verifyMongoDbSinkRecords(RepetitionInfo info) {
-
-        //defer manually wait for data propagation before first read back
-        if(info.getCurrentRepetition() == 1) {
-            deferExecutionToWaitForDataPropagation(PROCESSING_DELAY_TIME_MILLIS);
+            PRODUCER.send(record, (RecordMetadata r, Exception exc) -> {
+                assertNull(exc, () -> "unexpected error while sending: " + tweet
+                        + " | exc: " + exc.getMessage()
+                );
+            });
         }
 
-        int tweetCnt = info.getCurrentRepetition();
-        MongoCollection<Document> col = MONGO_DATABASE.getCollection("e2e-test-collection");
-        List<Document> docs = new ArrayList<>();
+        deferExecutionToWaitForDataPropagation(PROCESSING_DELAY_TIME_MILLIS);
 
-        Document found = col.find(eq(tweetCnt)).first();
-        assertNotNull(found, "document having _id="+tweetCnt+" is expected to be found");
-        System.out.println("found = " + found);
+        for (int tid = 0; tid < numTestRecords; tid++) {
+            MongoCollection<Document> col = MONGO_DATABASE.getCollection("e2e-test-collection");
+            Document found = col.find(eq(tid)).first();
+            //NOTE: this only verifies whether a document with the expected _id field
+            //exists everything else isn't particularly interesting during E2E scenarios
+            //but instead rigorously checked within a bunch of specific unit tests
+            assertNotNull(found, "document having _id=" + tid + " is expected to be found");
+            System.out.println("found = " + found);
+        }
 
     }
 
@@ -167,6 +182,15 @@ public class ContainerTest {
         try {
             Thread.sleep(PROCESSING_DELAY_TIME_MILLIS);
         } catch (InterruptedException e) {}
+    }
+
+    private static String extractHostnameFromDockerCompose(Map compose,String serviceName) {
+        return (String)((Map)((Map)compose.get("services")).get(serviceName)).get("hostname");
+    }
+
+    private static int extractHostPortFromDockerCompose(Map compose,String serviceName) {
+        return Integer.parseInt(((String)((List)((Map)((Map)compose.get("services"))
+                .get(serviceName)).get("ports")).get(0)).split(":")[1]);
     }
 
 }
