2252860e7afba0b6eaabd79120bdedc561d04f62
Ross Lawley <ross.lawley@gmail.com>
2019-03-05 13:42:51 +0000

    License headers added or updated

--- src/main/java/at/grahsl/kafka/connect/mongodb/MongoDbSinkRecordBatches.java
+++ src/main/java/at/grahsl/kafka/connect/mongodb/MongoDbSinkRecordBatches.java
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2018. Hans-Peter Grahsl (grahslhp@gmail.com)
+ * Copyright 2008-present MongoDB, Inc.
+ * Copyright 2017 Hans-Peter Grahsl.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
