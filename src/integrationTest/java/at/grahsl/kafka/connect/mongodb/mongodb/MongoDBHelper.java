1e227233e3a9b72e2c97ec05ef377aa4a8aaef04
Ross Lawley <ross.lawley@gmail.com>
2019-03-08 12:53:50 +0000

    Updated file  headers
    KAFKA-5

--- src/integrationTest/java/at/grahsl/kafka/connect/mongodb/mongodb/MongoDBHelper.java
+++ src/integrationTest/java/at/grahsl/kafka/connect/mongodb/mongodb/MongoDBHelper.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package at.grahsl.kafka.connect.mongodb.mongodb;
 
 import com.mongodb.ConnectionString;
--- src/main/java/at/grahsl/kafka/connect/mongodb/processor/id/strategy/ProvidedInKeyStrategy.java
+++ src/main/java/at/grahsl/kafka/connect/mongodb/processor/id/strategy/ProvidedInKeyStrategy.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2008-present MongoDB, Inc.
- * Copyright 2017 Hans-Peter Grahsl.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Original Work: Apache License, Version 2.0, Copyright 2017 Hans-Peter Grahsl.
  */
 
 package at.grahsl.kafka.connect.mongodb.processor.id.strategy;
