b2ab77c39c82686584b0f38567704d219484bbef
Ross Lawley <ross.lawley@gmail.com>
2019-04-30 10:31:44 +0100

    Simple MongoDB & Kafka Sink and Source example
    Uses Docker to create the following systems:
    
     * Zookeeper
     * Kafka
     * Confluent Schema Registry
     * Confluent Kafka Connect
     * Kafka Rest Proxy
     * Kafka Topics UI
     * MongoDB 3 node replicaset
    
    The `run.sh` script loads data into the `source` topic. Stores it in the
    `test.sink` collection and publishes the change stream events in the
    `mongodb.test.sink` topic.
    
    KAFKA-24

---
+++ docker/README.md
@@ -0,0 +1,44 @@
+# MongoDB & Kafka Docker end to end example
+
+A simple example that takes JSON documents from the `source` topic and stores them into the `test.sink` collection in MongoDB using 
+the MongoDB Kafka Sink Connector. 
+
+The MongoDB Kafka Source Connector also publishes all change stream events from `test.sink` into the `mongo.test.sink` topic.
+
+## Requirements
+  - Docker 18.09+
+  - Docker compose 1.24+
+  - *nix system
+
+## Running the example
+
+To run the example: `./run.sh` which will:
+  
+  - Run `docker-compose up` 
+  - Wait for MongoDB, Kafka, Kafka Connect to be ready
+  - Register the MongoDB Kafka Sink Connector
+  - Register the MongoDB Kafka Source Connector
+  - Publish some events to Kafka
+  - Write the events to MongoDB  
+  - Write the change stream messages to Kafka
+
+
+Once running, examine the topics in the Kafka UI: http://localhost:8000/
+  - The `source` topic should contain the 10 simple documents added.
+  - The `mongo.test.sink` should contain the change events.
+
+Examine the collections in MongoDB:
+  - In your shell run: docker-compose exec mongo1 /usr/bin/mongo
+  - Adding, updating data in `test.sink` - will show up as change stream events in the `mongo.test.sunk` topic
+
+## docker-compose.yml
+
+The following systems will be created:
+
+  - Zookeeper
+  - Kafka
+  - Confluent Schema Registry
+  - Confluent Kafka Connect
+  - Kafka Rest Proxy
+  - Kafka Topics UI
+  - MongoDB - a 3 node replicaset
---
+++ docker/run.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+echo "Starting docker ."
+docker-compose up -d --build
+
+sleep 5
+echo -ne "\n\nWaiting for the systems to be ready.."
+until $(curl --output /dev/null --silent --head --fail http://localhost:8082); do
+    printf '.'
+    sleep 1
+done
+
+until $(curl --output /dev/null --silent --head --fail http://localhost:18083); do
+    printf '.'
+    sleep 1
+done
+
+echo -e "\nConfiguring the MongoDB ReplicaSet.\n"
+docker-compose exec mongo1 /usr/bin/mongo --eval '''if (rs.status()["ok"] == 0) {
+    rsconf = {
+      _id : "rs0",
+      members: [
+        { _id : 0, host : "mongo1:27017", priority: 1.0 },
+        { _id : 1, host : "mongo2:27017", priority: 0.5 },
+        { _id : 2, host : "mongo3:27017", priority: 0.5 }
+      ]
+    };
+    rs.initiate(rsconf);
+}
+
+rs.conf();'''
+
+function finish {
+    curl --output /dev/null -X DELETE http://localhost:18083/connectors/mongo-sink
+    curl --output /dev/null -X DELETE http://localhost:18083/connectors/mongo-source
+    docker-compose exec mongo1 /usr/bin/mongo --eval "db.dropDatabase()"
+    docker-compose down
+    echo -e "Bye!\n"
+}
+trap finish EXIT
+
+echo -e "\nKafka Topics:"
+curl -X GET "http://localhost:8082/topics" -w "\n"
+
+echo -e "\nKafka Connectors:"
+curl -X GET "http://localhost:18083/connectors/" -w "\n"
+
+echo -e "\nAdding MongoDB Kafka Sink Connector for the 'source' topic into the 'test.source' collection:"
+curl -X POST -H "Content-Type: application/json" --data '
+  {"name": "mongo-sink",
+   "config": {
+     "connector.class":"com.mongodb.kafka.connect.MongoSinkConnector",
+     "tasks.max":"1",
+     "topics":"source",
+     "connection.uri":"mongodb://mongo1:27017,mongo2:27017,mongo3:27017",
+     "database":"test",
+     "collection":"sink",
+     "key.converter":"org.apache.kafka.connect.json.JsonConverter",
+     "key.converter.schemas.enable":false,
+     "value.converter":"org.apache.kafka.connect.json.JsonConverter",
+     "value.converter.schemas.enable":false
+}}' http://localhost:18083/connectors -w "\n"
+
+echo -e "\nAdding MongoDB Kafka Source Connector for the 'test.sink' collection:"
+curl -X POST -H "Content-Type: application/json" --data '
+  {"name": "mongo-source",
+   "config": {
+     "tasks.max":"1",
+     "connector.class":"com.mongodb.kafka.connect.MongoSourceConnector",
+     "connection.uri":"mongodb://mongo1:27017,mongo2:27017,mongo3:27017",
+     "topic.prefix":"mongo",
+     "database":"test",
+     "collection":"sink"
+}}' http://localhost:18083/connectors -w "\n"
+
+sleep 2
+
+echo -e "\nKafka Connectors: \n"
+curl -X GET "http://localhost:18083/connectors/" -w "\n"
+
+echo "Looking at data in `db.sink`:"
+docker-compose exec mongo1 /usr/bin/mongo --eval "db.sink.find()"
+
+echo -e "\nAdding some data to the 'source' topic \n"
+curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" \
+          --data '''{"records":[{"value":{"i":"0"}}, {"value":{"i":"1"}}, {"value":{"i":"2"}}, {"value":{"i":"3"}},
+           {"value":{"i":"4"}}, {"value":{"i":"5"}}, {"value":{"i":"6"}}, {"value":{"i":"7"}}, {"value":{"i":"8"}},
+           {"value":{"i":"9"}}]}''' \
+          "http://localhost:8082/topics/source"
+
+sleep 5
+echo "Looking at data in `db.sink`:"
+docker-compose exec mongo1 /usr/bin/mongo --eval "db.sink.find()"
+
+echo -e '''
+
+==============================================================================================================
+Examine the topics in the Kafka UI: http://localhost:8000/
+  - The `source` topic should contain the 10 simple documents added.
+  - The `mongo.test.sink` should contain the change events.
+
+Examine the collections:
+  - In your shell run: docker-compose exec mongo1 /usr/bin/mongo
+  - Adding, updating data in `test.sink` - will show up as change stream events in the `mongo.test.sunk` topic
+==============================================================================================================
+
+Use <ctrl>-c to quit'''
+
+read -r -d '' _ </dev/tty
