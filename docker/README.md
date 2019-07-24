23e0881200b03a91c62608814bdfbeefbff814cf
Ross Lawley <ross.lawley@gmail.com>
2019-07-24 11:33:30 +0100

    Updated the docker demo
    Now uses the datagen connector to generate the data.
    Also adds the confluent-control-center so the kafka data can be
    examined there.
    
    KAFKA-36

--- docker/README.md
+++ docker/README.md
@@ -1,9 +1,9 @@
 # MongoDB & Kafka Docker end to end example
 
-A simple example that takes JSON documents from the `source` topic and stores them into the `test.sink` collection in MongoDB using 
+A simple example that takes JSON documents from the `pageviews` topic and stores them into the `test.pageviews` collection in MongoDB using 
 the MongoDB Kafka Sink Connector. 
 
-The MongoDB Kafka Source Connector also publishes all change stream events from `test.sink` into the `mongo.test.sink` topic.
+The MongoDB Kafka Source Connector also publishes all change stream events from `test.pageviews` into the `mongo.test.pageviews` topic.
 
 ## Requirements
   - Docker 18.09+
@@ -16,26 +16,27 @@ To run the example: `./run.sh` which will:
   
   - Run `docker-compose up` 
   - Wait for MongoDB, Kafka, Kafka Connect to be ready
+  - Register the Confluent Datagen Connector
   - Register the MongoDB Kafka Sink Connector
   - Register the MongoDB Kafka Source Connector
-  - Publish some events to Kafka
+  - Publish some events to Kafka via the Datagen connector
   - Write the events to MongoDB  
-  - Write the change stream messages to Kafka
+  - Write the change stream messages back into Kafka
 
 
-Once running, examine the topics in the Kafka UI: http://localhost:8000/
-  - The `source` topic should contain the 10 simple documents added. Each similar to:<br>
+Once running, examine the topics in the Kafka control center: http://localhost:9021/
+  - The `pageviews` topic should contain the 10 simple documents added. Each similar to:<br>
     ```json
-    {"i": "0"}
+    {"viewtime": {"$numberLong": "81"}, "pageid": "Page_1", "userid": "User_8"}
     ```
-  - The `mongo.test.sink` should contain the 10 change events. Each similar to:<br>
+  - The `mongo.test.pageviews` should contain the 10 change events. Each similar to:<br>
     ```json
-    {"_id": {"_data": "<resumeToken>"}, 
-     "operationType": "insert",
-     "ns": {"db": "test", "coll": "sink"},
-     "documentKey": {"_id": {"$oid": "5cc99f4893283d634cb3f59e"}},
-     "clusterTime": {"$timestamp": {"t": 1556717385, "i": 1}},
-     "fullDocument": {"_id": {"$oid": "5cc99f4893283d634cb3f59e"}, "i": "0"}}
+     {"_id": {"_data": "<resumeToken>"}, 
+      "operationType": "insert", 
+      "clusterTime": {"$timestamp": {"t": 1563461814, "i": 4}}, 
+      "fullDocument": {"_id": {"$oid": "5d3088b6bafa7829964150f3"}, "viewtime": {"$numberLong": "81"}, "pageid": "Page_1", "userid": "User_8"}, 
+      "ns": {"db": "test", "coll": "pageviews"}, 
+      "documentKey": {"_id": {"$oid": "5d3088b6bafa7829964150f3"}}} 
     ```
 
 Examine the collections in MongoDB:
@@ -50,6 +51,8 @@ The following systems will be created:
   - Kafka
   - Confluent Schema Registry
   - Confluent Kafka Connect
+  - Confluent Control Center
+  - Confluent KSQL Server
   - Kafka Rest Proxy
   - Kafka Topics UI
   - MongoDB - a 3 node replicaset
--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e
+(
+if lsof -Pi :27017 -sTCP:LISTEN -t >/dev/null ; then
+    echo "Please terminate the local mongod on 27017"
+    exit 1
+fi
+)
+
 echo "Building the MongoDB Kafka Connector"
 (
 cd ..
@@ -19,7 +27,7 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:8082); d
     sleep 1
 done
 
-until $(curl --output /dev/null --silent --head --fail http://localhost:18083); do
+until $(curl --output /dev/null --silent --head --fail http://localhost:8083); do
     printf '.'
     sleep 1
 done
@@ -40,8 +48,9 @@ docker-compose exec mongo1 /usr/bin/mongo --eval '''if (rs.status()["ok"] == 0) 
 rs.conf();'''
 
 function finish {
-    curl --output /dev/null -X DELETE http://localhost:18083/connectors/mongo-sink
-    curl --output /dev/null -X DELETE http://localhost:18083/connectors/mongo-source
+    curl --output /dev/null -X DELETE http://localhost:8083/connectors/datagen-pageviews
+    curl --output /dev/null -X DELETE http://localhost:8083/connectors/mongo-sink
+    curl --output /dev/null -X DELETE http://localhost:8083/connectors/mongo-source
     docker-compose exec mongo1 /usr/bin/mongo --eval "db.dropDatabase()"
     docker-compose down
     echo -e "Bye!\n"
@@ -52,25 +61,43 @@ echo -e "\nKafka Topics:"
 curl -X GET "http://localhost:8082/topics" -w "\n"
 
 echo -e "\nKafka Connectors:"
-curl -X GET "http://localhost:18083/connectors/" -w "\n"
+curl -X GET "http://localhost:8083/connectors/" -w "\n"
 
-echo -e "\nAdding MongoDB Kafka Sink Connector for the 'source' topic into the 'test.source' collection:"
+echo -e "\nAdding datagen pageviews:"
+curl -X POST -H "Content-Type: application/json" --data '
+  { "name": "datagen-pageviews",
+    "config": {
+      "connector.class": "io.confluent.kafka.connect.datagen.DatagenConnector",
+      "kafka.topic": "pageviews",
+      "quickstart": "pageviews",
+      "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+      "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+      "value.converter.schemas.enable": "false",
+      "producer.interceptor.classes": "io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor",
+      "max.interval": 200,
+      "iterations": 10000000,
+      "tasks.max": "1"
+}}' http://localhost:8083/connectors -w "\n"
+
+sleep 5
+
+echo -e "\nAdding MongoDB Kafka Sink Connector for the 'pageviews' topic into the 'test.pageviews' collection:"
 curl -X POST -H "Content-Type: application/json" --data '
   {"name": "mongo-sink",
    "config": {
      "connector.class":"com.mongodb.kafka.connect.MongoSinkConnector",
      "tasks.max":"1",
-     "topics":"source",
+     "topics":"pageviews",
      "connection.uri":"mongodb://mongo1:27017,mongo2:27017,mongo3:27017",
      "database":"test",
-     "collection":"sink",
-     "key.converter":"org.apache.kafka.connect.json.JsonConverter",
-     "key.converter.schemas.enable":false,
-     "value.converter":"org.apache.kafka.connect.json.JsonConverter",
-     "value.converter.schemas.enable":false
-}}' http://localhost:18083/connectors -w "\n"
+     "collection":"pageviews",
+     "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+     "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+     "value.converter.schemas.enable": "false"
+}}' http://localhost:8083/connectors -w "\n"
 
-echo -e "\nAdding MongoDB Kafka Source Connector for the 'test.sink' collection:"
+sleep 2
+echo -e "\nAdding MongoDB Kafka Source Connector for the 'test.pageviews' collection:"
 curl -X POST -H "Content-Type: application/json" --data '
   {"name": "mongo-source",
    "config": {
@@ -79,38 +106,27 @@ curl -X POST -H "Content-Type: application/json" --data '
      "connection.uri":"mongodb://mongo1:27017,mongo2:27017,mongo3:27017",
      "topic.prefix":"mongo",
      "database":"test",
-     "collection":"sink"
-}}' http://localhost:18083/connectors -w "\n"
+     "collection":"pageviews"
+}}' http://localhost:8083/connectors -w "\n"
 
 sleep 2
-
 echo -e "\nKafka Connectors: \n"
-curl -X GET "http://localhost:18083/connectors/" -w "\n"
+curl -X GET "http://localhost:8083/connectors/" -w "\n"
 
-echo "Looking at data in `db.sink`:"
-docker-compose exec mongo1 /usr/bin/mongo --eval "db.sink.find()"
+echo "Looking at data in 'db.pageviews':"
+docker-compose exec mongo1 /usr/bin/mongo --eval 'db.pageviews.find()'
 
-echo -e "\nAdding some data to the 'source' topic \n"
-curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" \
-          --data '''{"records":[{"value":{"i":"0"}}, {"value":{"i":"1"}}, {"value":{"i":"2"}}, {"value":{"i":"3"}},
-           {"value":{"i":"4"}}, {"value":{"i":"5"}}, {"value":{"i":"6"}}, {"value":{"i":"7"}}, {"value":{"i":"8"}},
-           {"value":{"i":"9"}}]}''' \
-          "http://localhost:8082/topics/source"
-
-sleep 5
-echo "Looking at data in `db.sink`:"
-docker-compose exec mongo1 /usr/bin/mongo --eval "db.sink.find()"
 
 echo -e '''
 
 ==============================================================================================================
-Examine the topics in the Kafka UI: http://localhost:8000/
-  - The `source` topic should contain the 10 simple documents added.
-  - The `mongo.test.sink` should contain the change events.
+Examine the topics in the Kafka UI: http://localhost:9021 or http://localhost:8000/
+  - The `pageviews` topic should have the generated page views.
+  - The `mongo.test.pageviews` topic should contain the change events.
+  - The `test.pageviews` collection in MongoDB should contain the sinked page views.
 
 Examine the collections:
   - In your shell run: docker-compose exec mongo1 /usr/bin/mongo
-  - Adding, updating data in `test.sink` - will show up as change stream events in the `mongo.test.sunk` topic
 ==============================================================================================================
 
 Use <ctrl>-c to quit'''
