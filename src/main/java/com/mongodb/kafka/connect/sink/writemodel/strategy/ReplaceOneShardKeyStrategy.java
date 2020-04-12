0fd710deb814f212fdd119e2b46a150e84acf917
pat <patwakeem@gmail.com>
2020-04-12 21:22:13 +0200

    Set 4 spaces in ReplaceOneShardKeyStrategy

--- src/main/java/com/mongodb/kafka/connect/sink/writemodel/strategy/ReplaceOneShardKeyStrategy.java
+++ src/main/java/com/mongodb/kafka/connect/sink/writemodel/strategy/ReplaceOneShardKeyStrategy.java
@@ -14,31 +14,31 @@ import com.mongodb.kafka.connect.sink.converter.SinkDocument;
 
 public class ReplaceOneShardKeyStrategy implements WriteModelStrategy {
 
-  private static final ReplaceOptions REPLACE_OPTIONS = new ReplaceOptions().upsert(true);
+    private static final ReplaceOptions REPLACE_OPTIONS = new ReplaceOptions().upsert(true);
 
-  private String[] shardKeys;
+    private String[] shardKeys;
 
-  @Override
-  public WriteModel<BsonDocument> createWriteModel(final SinkDocument document) {
-    BsonDocument vd = document.getValueDoc().orElseThrow(
-        () -> new DataException("Error: cannot build the WriteModel since the value document was missing unexpectedly"));
+    @Override
+    public WriteModel<BsonDocument> createWriteModel(final SinkDocument document) {
+        BsonDocument vd = document.getValueDoc().orElseThrow(
+                () -> new DataException(
+                        "Error: cannot build the WriteModel since the value document was missing unexpectedly"));
+        final BsonDocument shardKeyTargetQuery = new BsonDocument();
 
-    final BsonDocument shardKeyTargetQuery = new BsonDocument();
+        for (String shardKey : shardKeys) {
+            final BsonValue shardKeyValue = vd.get(shardKey);
 
-    for (String shardKey : shardKeys) {
-      final BsonValue shardKeyValue = vd.get(shardKey);
+            if (shardKeyValue == null) {
+                throw new DataException("Value document does not contain required shard key: " + shardKey);
+            }
 
-      if (shardKeyValue == null) {
-        throw new DataException("Value document does not contain required shard key: " + shardKey);
-      }
+            shardKeyTargetQuery.put(shardKey, shardKeyValue);
+        }
 
-      shardKeyTargetQuery.put(shardKey, shardKeyValue);
+        return new ReplaceOneModel<>(shardKeyTargetQuery, vd, REPLACE_OPTIONS);
     }
 
-    return new ReplaceOneModel<>(shardKeyTargetQuery, vd, REPLACE_OPTIONS);
-  }
-
-  public void setShardKeys(String[] shardKeys) {
-    this.shardKeys = shardKeys;
-  }
+    public void setShardKeys(String[] shardKeys) {
+        this.shardKeys = shardKeys;
+    }
 }
