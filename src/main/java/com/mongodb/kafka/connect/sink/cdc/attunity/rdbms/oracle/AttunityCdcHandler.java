5be50367c08a59a8c99532ad4b23c4c1464d8c5b
Abraham Leal <aleal@confluent.io>
2020-05-13 10:03:41 -0500

    Add Attunity / Qlik replciate CDC support
    KAFKA-194

---
+++ src/main/java/com/mongodb/kafka/connect/sink/cdc/attunity/rdbms/oracle/AttunityCdcHandler.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mongodb.kafka.connect.sink.cdc.attunity.rdbms.oracle;
+
+import com.mongodb.kafka.connect.sink.MongoSinkTopicConfig;
+import com.mongodb.kafka.connect.sink.cdc.CdcHandler;
+import com.mongodb.kafka.connect.sink.cdc.CdcOperation;
+import com.mongodb.kafka.connect.sink.cdc.debezium.OperationType;
+import org.apache.kafka.connect.errors.DataException;
+import org.bson.BsonDocument;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AttunityCdcHandler extends CdcHandler {
+    private static final String OPERATION_TYPE_FIELD_PATH = "operation";
+    private static final String OPERATION_TYPE_WRAPPER_PATH = "headers";
+    private static final String OPERATION_TYPE_TOPLEVEL_WRAPPER_PATH = "message";
+
+    private final Map<OperationType, CdcOperation> operations = new HashMap<>();
+
+    public AttunityCdcHandler(final MongoSinkTopicConfig config) {
+        super(config);
+    }
+
+    protected void registerOperations(final Map<OperationType, CdcOperation> operations) {
+        this.operations.putAll(operations);
+    }
+
+    public CdcOperation getCdcOperation(final BsonDocument doc) {
+        if (doc.containsKey(OPERATION_TYPE_TOPLEVEL_WRAPPER_PATH)) {
+            try {
+                if (!doc.getDocument(OPERATION_TYPE_TOPLEVEL_WRAPPER_PATH)
+                        .getDocument(OPERATION_TYPE_WRAPPER_PATH)
+                        .containsKey(OPERATION_TYPE_FIELD_PATH)
+                        || !doc.getDocument(OPERATION_TYPE_TOPLEVEL_WRAPPER_PATH)
+                        .getDocument(OPERATION_TYPE_WRAPPER_PATH)
+                        .get(OPERATION_TYPE_FIELD_PATH)
+                        .isString()) {
+                    throw new DataException("Error: value doc is missing CDC operation type of type string");
+                }
+                String operation =
+                        getAttunityOperation(
+                                doc.getDocument(OPERATION_TYPE_TOPLEVEL_WRAPPER_PATH)
+                                        .getDocument(OPERATION_TYPE_WRAPPER_PATH)
+                                        .get(OPERATION_TYPE_FIELD_PATH)
+                                        .asString()
+                                        .getValue());
+                CdcOperation op = operations.get(OperationType.fromText(operation));
+                if (op == null) {
+                    throw new DataException(
+                            "Error: no CDC operation found in mapping for operation="
+                                    + doc.getDocument(OPERATION_TYPE_TOPLEVEL_WRAPPER_PATH)
+                                    .getDocument(OPERATION_TYPE_WRAPPER_PATH)
+                                    .get(OPERATION_TYPE_FIELD_PATH)
+                                    .asString()
+                                    .getValue());
+                }
+                return op;
+            } catch (IllegalArgumentException exc) {
+                throw new DataException("Error: parsing CDC operation failed", exc);
+            }
+        } else {
+            try {
+                if (!doc.getDocument(OPERATION_TYPE_WRAPPER_PATH).containsKey(OPERATION_TYPE_FIELD_PATH)
+                        || !doc.getDocument(OPERATION_TYPE_WRAPPER_PATH)
+                        .get(OPERATION_TYPE_FIELD_PATH)
+                        .isString()) {
+                    throw new DataException("Error: value doc is missing CDC operation type of type string");
+                }
+                String operation =
+                        getAttunityOperation(
+                                doc.getDocument(OPERATION_TYPE_WRAPPER_PATH)
+                                        .get(OPERATION_TYPE_FIELD_PATH)
+                                        .asString()
+                                        .getValue());
+                CdcOperation op = operations.get(OperationType.fromText(operation));
+                if (op == null) {
+                    throw new DataException(
+                            "Error: no CDC operation found in mapping for operation="
+                                    + doc.getDocument(OPERATION_TYPE_WRAPPER_PATH)
+                                    .get(OPERATION_TYPE_FIELD_PATH)
+                                    .asString()
+                                    .getValue());
+                }
+                return op;
+            } catch (IllegalArgumentException exc) {
+                throw new DataException("Error: parsing CDC operation failed", exc);
+            }
+        }
+    }
+
+    private String getAttunityOperation(String fromKey) {
+        switch(fromKey){
+            case "INSERT":
+            case "REFRESH":
+                return "c";
+            case "READ":
+                return "r";
+            case "UPDATE":
+                return "u";
+            case "DELETE":
+                return "d";
+            default:
+                throw new IllegalArgumentException("Error: unknown operation type " + fromKey);
+        }
+    }
+
+}
---
+++ src/main/java/com/mongodb/kafka/connect/sink/cdc/attunity/rdbms/oracle/AttunityRdbmsDelete.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mongodb.kafka.connect.sink.cdc.attunity.rdbms.oracle;
+
+import com.mongodb.client.model.DeleteOneModel;
+import com.mongodb.client.model.WriteModel;
+import com.mongodb.kafka.connect.sink.cdc.CdcOperation;
+import com.mongodb.kafka.connect.sink.cdc.debezium.OperationType;
+import com.mongodb.kafka.connect.sink.converter.SinkDocument;
+import org.apache.kafka.connect.errors.DataException;
+import org.bson.BsonDocument;
+
+public class AttunityRdbmsDelete implements CdcOperation {
+
+    @Override
+    public WriteModel<BsonDocument> perform(final SinkDocument doc) {
+
+        BsonDocument keyDoc =
+                doc.getKeyDoc()
+                        .orElseThrow(
+                                () -> new DataException("Error: key doc must not be missing for delete operation"));
+
+        BsonDocument valueDoc =
+                doc.getValueDoc()
+                        .orElseThrow(
+                                () -> new DataException("Error: value doc must not be missing for delete operation"));
+
+        try {
+            BsonDocument filterDoc =
+                    AttunityRdbmsHandler.generateFilterDoc(keyDoc, valueDoc, OperationType.DELETE);
+            return new DeleteOneModel<>(filterDoc);
+        } catch (Exception exc) {
+            throw new DataException(exc);
+        }
+
+    }
+
+}
