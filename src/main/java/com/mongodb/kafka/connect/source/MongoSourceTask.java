2e196b94e2704c051ced6ecd6db7a47f92e3cb1d
Nigel Liang <nigel@nigelliang.com>
2021-02-11 11:51:08 -0800

    KAFKA-216: throw when failing to resume change stream for any reason

--- src/main/java/com/mongodb/kafka/connect/source/MongoSourceTask.java
+++ src/main/java/com/mongodb/kafka/connect/source/MongoSourceTask.java
@@ -437,6 +437,8 @@ public final class MongoSourceTask extends SourceTask {
         if (resumeTokenNotFound(e)) {
           throw new ConnectException(
               "ResumeToken not found. Cannot create a change stream cursor", e);
+        } else {
+          throw new ConnectException("Failed to resume change stream", e);
         }
       }
       return null;
