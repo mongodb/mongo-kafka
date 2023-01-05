8e8a8ad342b31177afd91b32a98a6d430686fcb3
Ross Lawley <ross.lawley@gmail.com>
2023-01-05 10:07:29 +0000

    Remove duplicate test cases

--- src/test/java/com/mongodb/kafka/connect/source/schema/BsonDocumentToSchemaTest.java
+++ src/test/java/com/mongodb/kafka/connect/source/schema/BsonDocumentToSchemaTest.java
@@ -19,14 +19,6 @@ package com.mongodb.kafka.connect.source.schema;
 import static com.mongodb.kafka.connect.source.schema.BsonDocumentToSchema.DEFAULT_FIELD_NAME;
 import static com.mongodb.kafka.connect.source.schema.BsonDocumentToSchema.inferDocumentSchema;
 import static com.mongodb.kafka.connect.source.schema.SchemaUtils.assertSchemaEquals;
-import static java.util.Arrays.asList;
-import static org.apache.kafka.connect.data.Schema.Type.ARRAY;
-import static org.apache.kafka.connect.data.Schema.Type.BOOLEAN;
-import static org.apache.kafka.connect.data.Schema.Type.INT32;
-import static org.apache.kafka.connect.data.Schema.Type.STRING;
-import static org.apache.kafka.connect.data.Schema.Type.STRUCT;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.apache.kafka.connect.data.Decimal;
 import org.apache.kafka.connect.data.Schema;
@@ -34,12 +26,7 @@ import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Timestamp;
 import org.junit.jupiter.api.Test;
 
-import org.bson.BsonArray;
-import org.bson.BsonBoolean;
 import org.bson.BsonDocument;
-import org.bson.BsonInt32;
-import org.bson.BsonNull;
-import org.bson.BsonString;
 
 @SuppressWarnings("ArraysAsListWithZeroOrOneArgument")
 public class BsonDocumentToSchemaTest {
@@ -222,173 +209,6 @@ public class BsonDocumentToSchemaTest {
             .build();
 
     assertSchemaEquals(expected, inferDocumentSchema(bsonDocument));
-  }
-
-  @Test
-  void testEmptyArray() {
-    BsonDocument document =
-        new BsonDocument()
-            .append(
-                "outerArray",
-                new BsonArray(
-                    asList(
-                        new BsonDocument("innerArray", new BsonArray()),
-                        new BsonDocument(
-                            "innerArray",
-                            new BsonArray(asList(new BsonInt32(1), new BsonInt32(2)))),
-                        new BsonDocument(
-                            "innerArray",
-                            new BsonArray(asList(new BsonInt32(3), new BsonInt32(4)))))));
-
-    Schema schema = inferDocumentSchema(document);
-    assertEquals(STRUCT, schema.type());
-
-    Schema outerArraySchema = schema.field("outerArray").schema();
-    assertEquals(STRUCT, outerArraySchema.valueSchema().type());
-
-    Schema innerArraySchema = outerArraySchema.valueSchema().field("innerArray").schema();
-    assertEquals(ARRAY, innerArraySchema.type());
-    assertEquals(INT32, innerArraySchema.valueSchema().type());
-  }
-
-  @Test
-  void testStructCombining() {
-    BsonDocument document =
-        new BsonDocument()
-            .append(
-                "outerArray",
-                new BsonArray(
-                    asList(
-                        new BsonDocument()
-                            .append("c", new BsonString(""))
-                            .append("b", new BsonInt32(1)),
-                        new BsonDocument()
-                            .append("a", BsonBoolean.TRUE)
-                            .append("b", new BsonInt32(2)))));
-
-    Schema schema = inferDocumentSchema(document);
-    assertEquals(STRUCT, schema.type());
-
-    Schema outerArraySchema = schema.field("outerArray").schema();
-    assertEquals(STRUCT, outerArraySchema.valueSchema().type());
-
-    Schema outerArrayValueSchema = outerArraySchema.valueSchema();
-    assertEquals(STRUCT, outerArrayValueSchema.type());
-
-    assertEquals(BOOLEAN, outerArrayValueSchema.field("a").schema().type());
-    assertEquals(INT32, outerArrayValueSchema.field("b").schema().type());
-    assertEquals(STRING, outerArrayValueSchema.field("c").schema().type());
-
-    assertEquals("a", outerArrayValueSchema.fields().get(0).name());
-    assertEquals("b", outerArrayValueSchema.fields().get(1).name());
-    assertEquals("c", outerArrayValueSchema.fields().get(2).name());
-  }
-
-  @Test
-  void testStructCombiningNestedArrays() {
-    BsonDocument document =
-        new BsonDocument()
-            .append(
-                "outerArray",
-                new BsonArray(
-                    asList(
-                        new BsonDocument()
-                            .append(
-                                "innerArray",
-                                new BsonArray(
-                                    asList(
-                                        new BsonDocument()
-                                            .append("a", new BsonInt32(1))
-                                            .append("b", new BsonString("foo"))))),
-                        new BsonDocument()
-                            .append(
-                                "innerArray",
-                                new BsonArray(
-                                    asList(
-                                        new BsonDocument()
-                                            .append("b", new BsonString("foo"))
-                                            .append("c", new BsonInt32(2))))))));
-
-    Schema schema = inferDocumentSchema(document);
-    assertEquals(STRUCT, schema.type());
-
-    Schema outerArraySchema = schema.field("outerArray").schema();
-    assertEquals(STRUCT, outerArraySchema.valueSchema().type());
-
-    Schema outerArrayValueSchema = outerArraySchema.valueSchema();
-    assertEquals(STRUCT, outerArrayValueSchema.type());
-
-    Schema innerArraySchema = outerArrayValueSchema.field("innerArray").schema();
-    assertEquals(ARRAY, innerArraySchema.type());
-
-    Schema innerArrayValueSchema = innerArraySchema.valueSchema();
-    assertEquals(STRUCT, innerArrayValueSchema.type());
-
-    assertEquals(INT32, innerArrayValueSchema.field("a").schema().type());
-    assertEquals(STRING, innerArrayValueSchema.field("b").schema().type());
-    assertEquals(INT32, innerArrayValueSchema.field("c").schema().type());
-  }
-
-  @Test
-  void testStructCombiningWithNulls() {
-    BsonDocument document =
-        new BsonDocument()
-            .append(
-                "outerArray",
-                new BsonArray(
-                    asList(
-                        new BsonDocument()
-                            .append("a", new BsonInt32(1))
-                            .append("b", new BsonString("foo"))
-                            .append("c", BsonNull.VALUE)
-                            .append("d", BsonNull.VALUE)
-                            .append("e", new BsonArray()),
-                        new BsonDocument()
-                            .append("a", BsonNull.VALUE)
-                            .append("b", new BsonString("foo"))
-                            .append("c", new BsonInt32(2)))));
-
-    Schema schema = inferDocumentSchema(document);
-    assertEquals(STRUCT, schema.type());
-
-    Schema outerArraySchema = schema.field("outerArray").schema();
-    assertEquals(STRUCT, outerArraySchema.valueSchema().type());
-
-    Schema outerArrayValueSchema = outerArraySchema.valueSchema();
-    assertEquals(STRUCT, outerArrayValueSchema.type());
-
-    assertEquals(INT32, outerArrayValueSchema.field("a").schema().type());
-    assertEquals(STRING, outerArrayValueSchema.field("b").schema().type());
-    assertEquals(INT32, outerArrayValueSchema.field("c").schema().type());
-    assertEquals(STRING, outerArrayValueSchema.field("d").schema().type());
-    assertNull(outerArrayValueSchema.field("d").schema().defaultValue());
-    assertEquals(STRING, outerArrayValueSchema.field("e").schema().valueSchema().type());
-    assertNull(outerArrayValueSchema.field("e").schema().valueSchema().defaultValue());
-  }
-
-  @Test
-  void testUncombinableDocumentArrayElements() {
-    BsonDocument document =
-        new BsonDocument()
-            .append(
-                "outerArray",
-                new BsonArray(
-                    asList(
-                        new BsonDocument("a", new BsonArray(asList(new BsonInt32(1)))),
-                        new BsonDocument("a", new BsonArray(asList(new BsonString("foo")))))));
-
-    Schema schema = inferDocumentSchema(document);
-    assertEquals(STRUCT, schema.type());
-
-    Schema outerArraySchema = schema.field("outerArray").schema();
-    assertEquals(ARRAY, outerArraySchema.type());
-
-    Schema outerArrayValueSchema = outerArraySchema.valueSchema();
-    assertEquals(STRUCT, outerArrayValueSchema.type());
-
-    Schema fieldSchema = outerArrayValueSchema.field("a").schema();
-    assertEquals(ARRAY, fieldSchema.type());
-    assertEquals(STRING, fieldSchema.valueSchema().type());
   }
 
   static Schema createArray(final String name, final Schema valueSchema) {
