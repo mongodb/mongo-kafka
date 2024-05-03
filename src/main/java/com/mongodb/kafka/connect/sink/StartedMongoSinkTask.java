424b6839377f29da36b7f72df7f5c91b52b0b27a
Jagadish Nallapaneni <jagadish.nallapaneni@mongodb.com>
2024-05-02 21:19:36 -0700

    work in progress.

--- src/main/java/com/mongodb/kafka/connect/sink/StartedMongoSinkTask.java
+++ src/main/java/com/mongodb/kafka/connect/sink/StartedMongoSinkTask.java
@@ -30,6 +30,7 @@ import java.util.stream.Collectors;
 
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.errors.DataException;
+import org.apache.kafka.connect.errors.RetriableException;
 import org.apache.kafka.connect.sink.SinkRecord;
 
 import org.bson.BsonDocument;
@@ -109,6 +110,9 @@ final class StartedMongoSinkTask implements AutoCloseable {
       if (records.isEmpty()) {
         LOGGER.debug("No sink records to process for current poll operation");
       } else {
+        if (!mongoClient.getClusterDescription().hasWritableServer()) {
+          throw new RetriableException("database is not writable");
+        }
         Timer processingTime = Timer.start();
         List<List<MongoProcessedSinkRecordData>> batches =
             MongoSinkRecordProcessor.orderedGroupByTopicAndNamespace(
