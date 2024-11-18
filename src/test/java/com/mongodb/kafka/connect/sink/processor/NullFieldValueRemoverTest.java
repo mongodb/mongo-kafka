588301047b958eb565cebbee6da43f192b69d588
Calvin Thomas Nix <calvin.nix@mongodb.com>
2024-11-18 10:34:42 -0500

    fix checkstyles

--- src/test/java/com/mongodb/kafka/connect/sink/processor/NullFieldValueRemoverTest.java
+++ src/test/java/com/mongodb/kafka/connect/sink/processor/NullFieldValueRemoverTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.mongodb.kafka.connect.sink.processor;
 
 import static com.mongodb.kafka.connect.sink.SinkTestHelper.createTopicConfig;
@@ -120,7 +136,9 @@ public class NullFieldValueRemoverTest {
     new NullFieldValueRemover(createTopicConfig()).process(sinkDocWithValueDoc, null);
     BsonDocument expected =
         BsonDocument.parse(
-            "{'mySubDoc1': {'myDocumentString': 'a', 'mySubDoc2': {'myDocumentString': 'b', 'mySubDoc3': {'myDocumentString': 'c'}}}, 'myArray': [null, 'a', {}, 123], 'myDocument': {'myDocumentString': 'a'}, 'myDocumentAllNullFields': {}}");
+            "{'mySubDoc1': {'myDocumentString': 'a', 'mySubDoc2': {'myDocumentString': 'b', "
+                + "'mySubDoc3': {'myDocumentString': 'c'}}}, 'myArray': [null, 'a', {}, 123], "
+                + "'myDocument': {'myDocumentString': 'a'}, 'myDocumentAllNullFields': {}}");
 
     assertEquals(Optional.of(expected), sinkDocWithValueDoc.getValueDoc());
   }
