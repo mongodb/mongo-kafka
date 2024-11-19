a563e7b22d8f4123c9d6c98866a6c6ba4bec450f
Calvin Thomas Nix <calvin.nix@mongodb.com>
2024-11-19 09:49:56 -0500

    test formatting

--- src/test/java/com/mongodb/kafka/connect/sink/processor/NullFieldValueRemoverTest.java
+++ src/test/java/com/mongodb/kafka/connect/sink/processor/NullFieldValueRemoverTest.java
@@ -19,20 +19,12 @@ package com.mongodb.kafka.connect.sink.processor;
 import static com.mongodb.kafka.connect.sink.SinkTestHelper.createTopicConfig;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import org.bson.BsonArray;
-import org.bson.BsonBoolean;
 import org.bson.BsonDocument;
-import org.bson.BsonElement;
-import org.bson.BsonInt32;
-import org.bson.BsonNull;
-import org.bson.BsonString;
 
 import com.mongodb.kafka.connect.sink.converter.SinkDocument;
 
@@ -40,22 +32,29 @@ public class NullFieldValueRemoverTest {
   @Test
   @DisplayName("test NullFieldValueRemoverTest flat document")
   void testNullFieldValueRemoverFlatDocument() {
-    List<BsonElement> elements =
-        Arrays.asList(
-            new BsonElement("myNull1", new BsonNull()),
-            new BsonElement("myString", new BsonString("a")),
-            new BsonElement("myEmptyString", new BsonString("")),
-            new BsonElement("myNull2", new BsonNull()),
-            new BsonElement("myTrueBool", new BsonBoolean(true)),
-            new BsonElement("myFalseBool", new BsonBoolean(false)),
-            new BsonElement("myInt", new BsonInt32(123)),
-            new BsonElement("myNull3", new BsonNull()));
-    BsonDocument containsNullFieldValues = new BsonDocument(elements);
-    SinkDocument sinkDocWithValueDoc = new SinkDocument(null, containsNullFieldValues);
+    BsonDocument document =
+        BsonDocument.parse(
+            "{"
+                + "  'myNull1': null,"
+                + "  'myString': 'a',"
+                + "  'myEmptyString': ''"
+                + "  'myNull2': null,"
+                + "  'myTrueBool': true,"
+                + "  'myFalseBool': false,"
+                + "  'myInt': 123,"
+                + "  'myNull3': null"
+                + "}");
+    SinkDocument sinkDocWithValueDoc = new SinkDocument(null, document);
     new NullFieldValueRemover(createTopicConfig()).process(sinkDocWithValueDoc, null);
     BsonDocument expected =
         BsonDocument.parse(
-            "{'myString': 'a', 'myEmptyString': '', 'myTrueBool': true, 'myFalseBool': false, 'myInt': 123}");
+            "{"
+                + "  'myString': 'a',"
+                + "  'myEmptyString': '',"
+                + "  'myTrueBool': true,"
+                + "  'myFalseBool': false,"
+                + "  'myInt': 123"
+                + "}");
 
     assertEquals(Optional.of(expected), sinkDocWithValueDoc.getValueDoc());
   }
@@ -63,13 +62,9 @@ public class NullFieldValueRemoverTest {
   @Test
   @DisplayName("test NullFieldValueRemoverTest document with only null values")
   void testNullFieldValueRemoverObjectWithOnlyNullValues() {
-    List<BsonElement> elements =
-        Arrays.asList(
-            new BsonElement("myNull1", new BsonNull()),
-            new BsonElement("myNull2", new BsonNull()),
-            new BsonElement("myNull3", new BsonNull()));
-    BsonDocument containsOnlyNullFieldValues = new BsonDocument(elements);
-    SinkDocument sinkDocWithValueDoc = new SinkDocument(null, containsOnlyNullFieldValues);
+    BsonDocument document =
+        BsonDocument.parse("{ 'myNull1': null, 'myNull2': null, 'myNull3': null }");
+    SinkDocument sinkDocWithValueDoc = new SinkDocument(null, document);
     new NullFieldValueRemover(createTopicConfig()).process(sinkDocWithValueDoc, null);
     BsonDocument expected = BsonDocument.parse("{}");
     assertEquals(Optional.of(expected), sinkDocWithValueDoc.getValueDoc());
@@ -89,57 +84,27 @@ public class NullFieldValueRemoverTest {
   @Test
   @DisplayName("test NullFieldValueRemoverTest nested document")
   void testNullFieldValueRemoverNestedDocument() {
-    List<BsonElement> elements =
-        Arrays.asList(
-            new BsonElement("myNull1", new BsonNull()),
-            new BsonElement(
-                "mySubDoc1",
-                new BsonDocument(
-                    Arrays.asList(
-                        new BsonElement("myDocumentString", new BsonString("a")),
-                        new BsonElement("myDocumentNull", new BsonNull()),
-                        new BsonElement(
-                            "mySubDoc2",
-                            new BsonDocument(
-                                Arrays.asList(
-                                    new BsonElement("myDocumentString", new BsonString("b")),
-                                    new BsonElement("myDocumentNull", new BsonNull()),
-                                    new BsonElement(
-                                        "mySubDoc3",
-                                        new BsonDocument(
-                                            Arrays.asList(
-                                                new BsonElement(
-                                                    "myDocumentString", new BsonString("c")),
-                                                new BsonElement(
-                                                    "myDocumentNull", new BsonNull())))))))))),
-            new BsonElement(
-                "myArray",
-                new BsonArray(
-                    Arrays.asList(
-                        new BsonNull(),
-                        new BsonString("a"),
-                        new BsonDocument(
-                            Arrays.asList(
-                                new BsonElement("myArrayValueDocumentNull1", new BsonNull()),
-                                new BsonElement("myArrayValueDocumentNull2", new BsonNull()),
-                                new BsonElement("myArrayValueDocumentNull3", new BsonNull()))),
-                        new BsonInt32(123)))),
-            new BsonElement("myNull3", new BsonNull()),
-            new BsonElement(
-                "myDocument",
-                new BsonDocument(
-                    Arrays.asList(
-                        new BsonElement("myDocumentString", new BsonString("a")),
-                        new BsonElement("myDocumentNull", new BsonNull())))),
-            new BsonElement(
-                "myDocumentAllNullFields",
-                new BsonDocument(
-                    Arrays.asList(
-                        new BsonElement("myDocumentNull1", new BsonNull()),
-                        new BsonElement("myDocumentNull2", new BsonNull()),
-                        new BsonElement("myDocumentNull3", new BsonNull())))));
-    BsonDocument containsNullFieldValues = new BsonDocument(elements);
-    SinkDocument sinkDocWithValueDoc = new SinkDocument(null, containsNullFieldValues);
+    BsonDocument document =
+        BsonDocument.parse(
+            "{"
+                + "  'myNull1': null,"
+                + "  'mySubDoc1': {"
+                + "    'myDocumentString': 'a',"
+                + "    'myDocumentNull': null,"
+                + "    'mySubDoc2': {"
+                + "      'myDocumentString': 'b',"
+                + "      'myDocumentNull': null,"
+                + "      'mySubDoc3': {"
+                + "        'myDocumentString': 'c',"
+                + "        'myDocumentNull': null"
+                + "      }"
+                + "    }"
+                + "  },"
+                + "  'myArray': [null, 'a', { 'a': null, 'b': null, 'c': null }, 123],"
+                + "  'myNull2': null,"
+                + "  'myDocument': { 'myDocumentString': 'a', 'myDocumentNull': null }"
+                + "}");
+    SinkDocument sinkDocWithValueDoc = new SinkDocument(null, document);
     new NullFieldValueRemover(createTopicConfig()).process(sinkDocWithValueDoc, null);
     BsonDocument expected =
         BsonDocument.parse(
@@ -156,8 +121,7 @@ public class NullFieldValueRemoverTest {
                 + "  'myArray': [null, 'a', {}, 123],"
                 + "  'myDocument': {"
                 + "    'myDocumentString': 'a'"
-                + "  },"
-                + "  'myDocumentAllNullFields': {}"
+                + "  }"
                 + "}");
 
     assertEquals(Optional.of(expected), sinkDocWithValueDoc.getValueDoc());
